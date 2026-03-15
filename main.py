2b042feb712c42fea8596742a18bfd3f23efd8fc
Rezvon1409 <rezvon0880@gmail.com>
2026-03-15 14:21:32 +0500

    CRUD

--- main.py
+++ main.py
@@ -58,3 +58,69 @@ def logout(response: Response, request: Request):
         del sessions[session_id]
     response.delete_cookie("session_id")
     return {"message": "Logged out"}
+
+
+
+@app.get("/notes", response_model=list[NoteOut])
+def get_my_notes(request: Request, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    notes = db.query(Note).filter(Note.owner_id == user_id).all()
+    return notes
+
+@app.post("/notes", response_model=NoteOut)
+def create_note(request: Request, note_in: NoteCreate, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    new_note = Note(title=note_in.title, content=note_in.content, owner_id=user_id)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
+
+@app.get("/notes/{note_id}", response_model=NoteOut)
+def get_note(note_id: int, request: Request, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+@app.put("/notes/{note_id}", response_model=NoteOut)
+def update_note(note_id: int, note_in: NoteUpdate, request: Request, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+        
+    if note_in.title is not None:
+        note.title = note_in.title
+    if note_in.content is not None:
+        note.content = note_in.content
+        
+    db.commit()
+    db.refresh(note)
+    return note
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id: int, request: Request, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+        
+    db.delete(note)
+    db.commit()
+    return {"message": "Note deleted"}
+
+@app.get("/admin/notes", response_model=list[NoteOut])
+def get_all_notes_admin(request: Request, db: Session = Depends(get_db)):
+    user_id = get_current_user_id(request)
+    user = db.query(User).filter(User.id == user_id).first()
+    
+    if not user or not user.is_admin:
+        raise HTTPException(status_code=403, detail="Not an admin")
+        
+    notes = db.query(Note).all()
+    return notes
+
+if __name__ == '__main__':
+    uvicorn.run('main:app' , host='127.0.0.1' , port=8000 , reload=True)
